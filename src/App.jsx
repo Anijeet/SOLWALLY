5c344de20e467fc02a2321f6379e857773c3ab12
Anijeet <anijeetmani18@gmail.com>
2025-08-18 15:09:45 +0530

    created

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { FC, useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+
+// Default styles that can be overridden by your app
+import "@solana/wallet-adapter-react-ui/styles.css";
+import Airdrop from "./components/Airdrop";
+
+function App() {
+
+  //using alchemy rpc url
+  const endpoint="https://solana-devnet.g.alchemy.com/v2/QELskRlvP3MOxsoXjyqvp"
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          <WalletDisconnectButton />
+            <Airdrop/>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
+
+export default App;
